cc75b0fd07955e033997707b14e6d07b63a9ebf8
sanooj <146158357+sanooj-sahadevan@users.noreply.github.com>
2024-11-13 14:31:41 +0530

    hosting

--- src/index.ts
+++ src/index.ts
@@ -101,7 +101,7 @@ import logger from './utils/logger';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8082;
 const morganFormat = ":method :url :status :response-time ms";
 
 connectToMongoDB();
